81fff12127288871e2b9ea6f8b109a5c4d985e24
Kevin L <laikevin279@gmail.com>
2026-07-01 18:19:51 +0800

    K color fill

--- public/logo.js
+++ public/logo.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 const IconK = ({ className = "w-6 h-6", ...props }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -29,7 +27,7 @@ const IconK = ({ className = "w-6 h-6", ...props }) => (
       fontSize="12"
       fontWeight="700"
       fontFamily="Inter, system-ui, sans-serif"
-      fill="currentColor"
+      className="fill-teal-500"
     >
       K
     </text>
